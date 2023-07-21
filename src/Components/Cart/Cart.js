b89bf7c9c5ddb85b041df009588af5c100a89a2a
baikarrohit <baikarrohit1999@gmail.com>
2023-07-21 22:19:08 +0530

    created  cart component with hardcoded array

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,74 @@
-import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+const cartElements = [
+  {
+    title: "Colors",
+
+    price: 100,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+
+    quantity: 2,
+  },
+
+  {
+    title: "Black and white Colors",
+
+    price: 50,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+
+    quantity: 3,
+  },
+
+  {
+    title: "Yellow and Black Colors",
+
+    price: 70,
+
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+
+    quantity: 1,
+  },
+];
 
 const Cart = () => {
-    return (
-        <div>
-            <Button variant="outline-info">Cart</Button>
-        </div>
-    )
-}
+  const [show, setShow] = useState(false);
+  const closeHandler = () => {
+    setShow(false);
+  };
+
+  const showHandler = () => {
+    setShow(true);
+  };
+  return (
+    <div>
+      <Button variant="outline-info" onClick={showHandler}>
+        Cart
+      </Button>
+
+      <Modal show={show} onHide={closeHandler} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cart</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <ul>
+            {cartElements.map((cart) => (
+              <li className="list-unstyled">
+                {cart.title} - {cart.price} - {cart.quantity}{" "}
+                <Button variant="danger">Remove</Button>
+              </li>
+            ))}
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary">Purchase</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
 
 export default Cart;
