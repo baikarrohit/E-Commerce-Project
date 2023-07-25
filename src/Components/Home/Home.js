d1cf0a49d50985488d23a4ecd283528ca9d7b09c
baikarrohit <baikarrohit1999@gmail.com>
2023-07-25 16:03:55 +0530

    fetch movie on page load

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import classes from "./Home.module.css";
 
 const tourArr = [
@@ -12,11 +12,11 @@ const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/film/");
+      const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
         throw new Error("something went wrong...Retrying!");
       }
@@ -35,7 +35,13 @@ const Home = () => {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  
   return (
     <>
       <div className={classes.container}>
