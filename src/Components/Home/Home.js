1e993c495d64055c66e39ae730e0d178b794b45f
baikarrohit <baikarrohit1999@gmail.com>
2023-07-26 14:15:32 +0530

    Storing and deleting the new movies into the Databases using firebase

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -19,21 +19,24 @@ const Home = () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://ecommerce-http-bb6d7-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
         throw new Error("something went wrong...Retrying!");
       }
       const data = await response.json();
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingtext: movieData.opening_crawl,
-          releasedate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -44,15 +47,37 @@ const Home = () => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  const formSubmitHandler = (event) => {
+  const formSubmitHandler = async (event) => {
     event.preventDefault();
     const newMovieObj = {
       title: titleRef.current.value,
       openingText: textRef.current.value,
       releaseDate: dateRef.current.value,
     };
-    console.log(newMovieObj);
+    await fetch(
+      "https://ecommerce-http-bb6d7-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(newMovieObj),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   };
+
+  const movieDeleteHandler = async (id) => {
+    try {
+      await fetch(
+        `https://ecommerce-http-bb6d7-default-rtdb.firebaseio.com/movies/${id}.json`,
+        { method: "DELETE" }
+      );
+      setMovies((prevMovies) => prevMovies.filter((movie) => movie.id !== id));
+    } catch {
+      setError("Error while deleting the movie!");
+    }
+  };
+
   return (
     <>
       <div className={classes.container}>
@@ -77,10 +102,11 @@ const Home = () => {
             <label>title</label>
             <input type="text" ref={titleRef} />
             <label>Opening Text</label>
-            <textarea rows="5" ref={textRef} />
+            <textarea rows="3" ref={textRef} />
             <label>Release Date</label>
             <input type="text" ref={dateRef} />
             <button type="submit">Add Movie</button>
+            
           </form>
           <button onClick={fetchMoviesHandler}>Fetch Movies</button>
         </section>
@@ -88,10 +114,11 @@ const Home = () => {
           {!isLoading && (
             <ul>
               {movies.map((movie) => (
-                <li>
+                <li key={movie.id}>
                   <h2>{movie.title}</h2>
                   <h3>{movie.releasedate}</h3>
                   <p>{movie.openingtext}</p>
+                  <button onClick={() => movieDeleteHandler(movie.id)}>Delete Movie</button>
                 </li>
               ))}
             </ul>
